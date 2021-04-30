49c57c613cff0755bf088adb1ce4a3bb7d64ecd5
Your Name <you@example.com>
2021-04-30 16:46:30 -0400

    Fecha inicio contrato

--- models/models.py
+++ models/models.py
@@ -7,6 +7,7 @@ class Contrato(models.Model):
     _description = 'model.technical.name'
     
     pago_anual = fields.Boolean(string='Pago Anual')
+    fecha_inicio = fields.Date(string='Fecha Inicio Contrato')
     contract_total = fields.Float(string='Total del Contrato')
     
     @api.onchange('contract_line_ids','contract_line_ids.quantity','contract_line_ids.price_unit')
